3abe05bb8cd2db63ee923cf1562208cc3d0b65ab
maelseewal <maelseewald@gmail.com>
2024-11-20 13:56:16 +0100

    5

--- analogClock.js
+++ analogClock.js
@@ -1,34 +1,34 @@
-// Funktion, um die aktuelle Zeit zu erhalten und die Zeiger zu drehen
+//function for the time
 function updateClock() {
-  // Aktuelle Zeit holen
+  // take the time
   const now = new Date();
 
-  // Stunde, Minute und Sekunde ermitteln
+  // Variables for seconds minutes and hours
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  // Umrechnung der Zeit auf Grad
+  //time in degres
   const hourDeg = (hours % 12) * 30 + (minutes / 60) * 30; // 360 Grad / 12 Stunden = 30 Grad pro Stunde
   const minuteDeg = minutes * 6 + (seconds / 60) * 6; // 360 Grad / 60 Minuten = 6 Grad pro Minute
   const secondDeg = seconds * 6; // 360 Grad / 60 Sekunden = 6 Grad pro Sekunde
 
-  // Zeiger-Elemente holen
+  // Connect with pointer elements
   const hourHand = document.querySelector(".hour-hand");
   const minuteHand = document.querySelector(".min-hand");
   const secondHand = document.querySelector(".second-hand");
 
-  // Drehung der Zeiger anwenden
+  // Rotation of the pointer
   hourHand.style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
   minuteHand.style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
   secondHand.style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
 }
 
 // Funktion aufrufen, um die Uhr zu starten
-setInterval(updateClock, 1000);
+setInterval(updateClock, 1);
 const clock = document.querySelector(".clock");
 
-//? Add hour markings
+// Add hour markings
 for (let i = 0; i < 12; i++) {
   const hourMark = document.createElement("div");
   hourMark.classList.add("marking");
@@ -36,7 +36,7 @@ for (let i = 0; i < 12; i++) {
   clock.appendChild(hourMark);
 }
 
-//? Adjust the hour markings
+// Adjust the hour markings
 for (let i = 0; i < 12; i++) {
   const hourMark = document.createElement("div");
   hourMark.classList.add("adjustMarking");
@@ -44,7 +44,7 @@ for (let i = 0; i < 12; i++) {
   clock.appendChild(hourMark);
 }
 
-//? Add minute markings
+// Add minute markings
 for (let i = 0; i < 60; i++) {
   if (i % 5 !== 0) {
     const minuteMark = document.createElement("div");
@@ -54,7 +54,7 @@ for (let i = 0; i < 60; i++) {
   }
 }
 
-//? Adjust the minute markings
+// Adjust the minute markings
 for (let i = 0; i < 60; i++) {
   if (i % 5 !== 0) {
     const minuteMark = document.createElement("div");
